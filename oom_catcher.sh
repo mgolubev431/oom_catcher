4abeee89334accbfcf2927a9f605f18f7c3b2d20
Kamil <149625264+bayshihin@users.noreply.github.com>
2024-04-16 01:27:51 +0500

    Create oom_catcher.sh

---
+++ oom_catcher.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Get the current time in timestamp format
+start_time=$(date +%s)
+
+# Start an endless loop
+while true
+do
+    # Check that a certain amount of time has passed since the script was launched (for example, 30 seconds)
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+
+    if [ $elapsed_time -ge 30 ]; then
+        # Check the log for entries from OOM-killer
+        if grep -q "oom-killer" /var/log/syslog; then
+            echo "OOM-killer активирован. Останавливаем скрипт."
+            break  # Break the loop if an entry from OOM-killer is found
+        fi
+    fi
+
+    # Start the stress process
+    stress --vm 1 --vm-bytes 128M --vm-hang 0 &
+
+    sleep 3  # Wait 3 seconds before next iteration
+done
+
+# After exiting the loop, kill all processes launched by the script
+pkill -f stress
